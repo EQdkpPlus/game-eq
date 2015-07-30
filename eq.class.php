26f70ff53c9d74eede6985426c80eab299246591
darkmaeg <dinodox@yahoo.com>
2015-07-29 22:17:39 -0400

    Update eq.class.php

--- eq.class.php
+++ eq.class.php
@@ -51,23 +51,23 @@ if(!class_exists('eq')) {
 				'recruitment' => true,
 				'parent'	=> array(
 					'race' => array(
-						0	=> 'all',									// Unknown
-						1	=> array(4,5,7,8,10,11,13,14,15),			// Gnome
-						2	=> array(1,4,5,6,7,8,9,10,11,13,14,15,16),	// Human
-						3	=> array(2,3,10,12,13),						// Barbarian
-						4	=> array(3,15,8,10,13),						// Dwarf
-						5	=> array(15,4,5,8,14),						// High Elf
-						6	=> array(4,5,7,10,11,13,14,15),				// Dark Elf
-						7	=> array(1,2,16,9,10,13),					// Wood Elf
-						8	=> array(1,4,8,9,10,13),					// Half Elf
-						9	=> array(1,2,3,10,12,13),					// Vah Shir
-						10	=> array(2,3,11,12,13),						// Troll
-						11	=> array(2,3,11,12,13),						// Ogre
-						12	=> array(15,6,7,8,10,11,12,13,14),			// Froglok
-						13	=> array(2,6,7,11,12,13),					// Iksar
-						14	=> array(15,4,5,7,8,11,14),					// Erudite
-						15	=> array(15,16,8,9,10,13),					// Halfling
-						16	=> array(1,15,16,4,5,6,7,8,9,10,11,13,14),	// Drakkin
+						0 => 'all', // Unknown
+						1 => array(15,4,5,7,8,10,11,13,14), // Gnome
+						2 => array(1,15,16,4,5,6,7,8,9,10,11,13,14), // Human
+						3 => array(2,3,10,12,13), // Barbarian
+						4 => array(3,15,8,10,13), // Dwarf
+						5 => array(15,4,5,8,14), // High Elf
+						6 => array(15,4,5,7,10,11,13,14), // Dark Elf
+						7 => array(1,2,16,9,10,13), // Wood Elf
+						8 => array(1,16,8,9,10,13), // Half Elf
+						9 => array(1,2,10,12,13), // Vah Shir
+						10 => array(2,3,11,12,13), // Troll
+						11 => array(2,3,11,12,13), // Ogre
+						12 => array(15,6,7,8,10,11,12,13,14), // Froglok
+						13 => array(2,6,7,11,12,13), // Iksar
+						14 => array(15,4,5,7,8,11,14), // Erudite
+						15 => array(15,16,8,9,10,13), // Halfling
+						16 => array(1,15,16,4,5,6,7,8,9,10,11,13,14), // Drakkin
 					),
 				),
 			),
