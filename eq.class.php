0da2ab1c6af973578945395c80cbb42f69f4d8d3
God Mod <godmod23@web.de>
2015-09-23 12:01:21 +0200

    Fixes

--- eq.class.php
+++ eq.class.php
@@ -51,23 +51,23 @@ if(!class_exists('eq')) {
 				'recruitment' => true,
 				'parent'	=> array(
 					'race' => array(
-						0 => 'all', // Unknown
-						1 => array(15,4,5,7,8,10,11,13,14), // Gnome
-						2 => array(1,15,16,4,5,6,7,8,9,10,11,13,14), // Human
-						3 => array(2,3,10,12,13), // Barbarian
-						4 => array(3,15,8,10,13), // Dwarf
-						5 => array(15,4,5,8,14), // High Elf
-						6 => array(15,4,5,7,10,11,13,14), // Dark Elf
-						7 => array(1,2,16,9,10,13), // Wood Elf
-						8 => array(1,16,8,9,10,13), // Half Elf
-						9 => array(1,2,10,12,13), // Vah Shir
-						10 => array(2,3,11,12,13), // Troll
-						11 => array(2,3,11,12,13), // Ogre
-						12 => array(15,6,7,8,10,11,12,13,14), // Froglok
-						13 => array(2,6,7,11,12,13), // Iksar
-						14 => array(15,4,5,7,8,11,14), // Erudite
-						15 => array(15,16,8,9,10,13), // Halfling
-						16 => array(1,15,16,4,5,6,7,8,9,10,11,13,14), // Drakkin
+						0	=> 'all',									// Unknown
+						1	=> array(4,5,7,8,10,11,13,14,15),			// Gnome
+						2	=> array(1,4,5,6,7,8,9,10,11,13,14,15,16),	// Human
+						3	=> array(2,3,10,12,13),						// Barbarian
+						4	=> array(3,15,8,10,13),						// Dwarf
+						5	=> array(15,4,5,8,14),						// High Elf
+						6	=> array(4,5,7,10,11,13,14,15),				// Dark Elf
+						7	=> array(1,2,16,9,10,13),					// Wood Elf
+						8	=> array(1,8,9,10,13,16),					// Half Elf
+						9	=> array(1,2,3,10,12,13),					// Vah Shir
+						10	=> array(2,3,11,12,13),						// Troll
+						11	=> array(2,3,11,12,13),						// Ogre
+						12	=> array(15,6,7,8,10,11,12,13,14),			// Froglok
+						13	=> array(2,6,7,11,12,13),					// Iksar
+						14	=> array(15,4,5,7,8,11,14),					// Erudite
+						15	=> array(15,16,8,9,10,13),					// Halfling
+						16	=> array(1,15,16,4,5,6,7,8,9,10,11,13,14),	// Drakkin
 					),
 				),
 			),
@@ -88,10 +88,27 @@ if(!class_exists('eq')) {
 				$names = $this->classes[$this->lang];
 				$this->filters[$lang] = array(
 					array('name' => '-----------', 'value' => false),
-					array('name' => $this->glang('plate', true, $lang), 'value' => array(1 => 'class', 8 => 'class', 11 => 'class', 13 => 'class', 15 => 'class')),
-					array('name' => $this->glang('chain', true, $lang), 'value' => array(3 => 'class', 9 => 'class', 10 => 'class', 12 => 'class')),
-					array('name' => $this->glang('leather', true, $lang), 'value' => array(2 => 'class', 6 => 'class', 16 => 'class')),
-					array('name' => $this->glang('silk', true, $lang), 'value' => array(4 => 'class', 5 => 'class', 7 => 'class', 14 => 'class')),
+					array('name' => $this->glang('tank', true, $lang), 'value' => 'class:8,11,13'),
+					array('name' => $this->glang('melee', true, $lang), 'value' => 'class:1,2,3,6,9,10'),
+					array('name' => $this->glang('priest', true, $lang), 'value' => 'class:12,15,16'),
+					array('name' => $this->glang('caster', true, $lang), 'value' => 'class:4,5,7,14'),
+					array('name' => '-----------', 'value' => false),
+					array('name' => $this->glang('Bard', true, $lang), 'value' => 'class:1'),
+					array('name' => $this->glang('Beastlord', true, $lang), 'value' => 'class:2'),
+					array('name' => $this->glang('Berserker', true, $lang), 'value' => 'class:3'),
+					array('name' => $this->glang('Cleric', true, $lang), 'value' => 'class:15'),
+					array('name' => $this->glang('Druid', true, $lang), 'value' => 'class:16'),
+					array('name' => $this->glang('Enchanter', true, $lang), 'value' => 'class:4'),
+					array('name' => $this->glang('Magician', true, $lang), 'value' => 'class:5'),
+					array('name' => $this->glang('Monk', true, $lang), 'value' => 'class:6'),
+					array('name' => $this->glang('Necromancer', true, $lang), 'value' => 'class:7'),
+					array('name' => $this->glang('Paladin', true, $lang), 'value' => 'class:8'),
+					array('name' => $this->glang('Ranger', true, $lang), 'value' => 'class:9'),
+					array('name' => $this->glang('Rogue', true, $lang), 'value' => 'class:10'),
+					array('name' => $this->glang('Shadowknight', true, $lang), 'value' => 'class:11'),
+					array('name' => $this->glang('Shaman', true, $lang), 'value' => 'class:12'),
+					array('name' => $this->glang('Warrior', true, $lang), 'value' => 'class:13'),
+					array('name' => $this->glang('Wizard', true, $lang), 'value' => 'class:14'),
 				);
 			}
 		}
